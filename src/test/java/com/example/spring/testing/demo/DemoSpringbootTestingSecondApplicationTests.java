4c7aed26c2b792e4f75d38240c6d3f5c6484f395
piero <piero@Linux-Mint-Peschereccio>
2021-06-08 16:20:45 +0200

    New testing project

---
+++ src/test/java/com/example/spring/testing/demo/DemoSpringbootTestingSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.testing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootTestingSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
